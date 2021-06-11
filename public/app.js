3d39fecae344f7d3f76009e92f13630d19e12926
ELBO0527 <63278770+ELBO0527@users.noreply.github.com>
2021-06-11 16:29:38 +0900

    Update app.js

--- public/app.js
+++ public/app.js
@@ -9,14 +9,15 @@ const create = document.querySelector('#create-form');
       create.addEventListener('submit', (e)=>{
         e.preventDefault();
 
-        db.collection("board").add({
+        db.collection("show_info").add({
           title : create.title.value,
-          content : create.content.value,
+          notice : create.notice.value,
           runtime : Number(create.runtime.value),
           startday : create.startday.value,
           finishday : create.finishday.value,
           state : create.state.value,
-          hit : 0
+          hit : 0,
+          image_Path : "lamancha_3.PNG"
       })
       alert('등록이 완료되었습니다.')
     })
@@ -30,7 +31,7 @@ const create = document.querySelector('#create-form');
 //ref.doc("abcd1234").delete();
 
 
-let ref1 = db.collection("board");
+let ref1 = db.collection("show_info");
 let getDoc = ref1.get()
     .then(doc => {
         doc.forEach( item=>{  //item이 저장한 데이터 객체 원본입니다.
